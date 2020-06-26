3e25a03e0d3be21f78e309e85f2757768e1d3acc
Phil Wells <phil.wells@nytimes.com>
2020-06-26 15:06:21 -0400

    unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, defaultProps } from 'react'
+import React from 'react'
 import { render, Text } from 'ink'
 
 export const App = ({ state = { title: 'TDD Roguelike!' } }) => {
